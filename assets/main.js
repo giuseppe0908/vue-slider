3a2596bb8c706259bb97cf87bb5c71f549836bb9
giuseppe <placidagiuseppe@gmail.com>
2021-03-24 13:33:50 +0100

    struttura html e css fatta

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+var app = new Vue(
+  {
+    el:'#root',
+    data:{
+      i:0,
+      img_array:[
+        "https://images-na.ssl-images-amazon.com/images/I/5199%2B%2B-QxmL.jpg",
+        "https://librisognierealta.it/wp-content/uploads/2017/06/Harry-Potter-e-la-camera-dei-segreti-cover-locandina.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/81vteywKEKL._SL1441_.jpg",
+        "https://img.ibs.it/images/5051891142718_0_306_0_75.jpg",
+      ]
+    },
+  }
+);
